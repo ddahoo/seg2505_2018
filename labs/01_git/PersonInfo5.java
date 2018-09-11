ca7d14bd6fd64cc78651838d08daeea20679c571
Jean-Luc Blais-Amyot <jblai083@uottawa.ca>
2018-09-11 17:37:35 -0400

    2

--- labs/01_git/PersonInfo5.java
+++ labs/01_git/PersonInfo5.java
@@ -3,7 +3,7 @@ public class PersonInfo5 {
     public static String firstName = "Jean-Luc";
     public static String lastName = "Blais-Amyot";
     public static int studentNo = 300035269;
-    public static String description = "CompSci";
+    public static String description = "CompSci.";
     
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
