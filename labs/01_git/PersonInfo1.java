2b8a5105fcfae743e80654922718cdda8caa3f0a
Lydia-Fen Leduc-Chapman <lledu102@uottawa.ca>
2018-09-11 17:12:31 -0400

    New infos

--- labs/01_git/PersonInfo1.java
+++ labs/01_git/PersonInfo1.java
@@ -3,7 +3,7 @@ public class PersonInfo1 {
     public static String firstName = "Lydia";
     public static String lastName = "Leduc";
     public static int studentNo = 8990797;
-    public static String description = "Loves to cook";
+    public static String description = "Loves to play";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
